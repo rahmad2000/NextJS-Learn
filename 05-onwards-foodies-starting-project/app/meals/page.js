f48514e75b543967e1ad73b9ea355a939728abbb
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-06-12 12:02:01 -0500

    S3 E107

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,7 +1,24 @@
-export default function Meals() {
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "../../components/meals/meals-grid";
+export default function MealsPage() {
   return (
-    <div>
-      <h1>Meals Page</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highligh}>by you</span>
+        </h1>
+        <p>
+          Choose you favorite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share you favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}></MealsGrid>
+      </main>
+    </>
   );
 }
